595ac93c2d3993d7c2e6109ffdd474eb5d436a07
AibekYrysbekov <yrysbekovayba@gmail.com>
2022-11-15 17:58:22 +0600

    open

---
+++ 123.py
@@ -0,0 +1,30 @@
+'''
+
+with open('directories.txt', 'a') as f:
+    f.write("IT/Видео/Загрузки/Музыка/Рабочий стол/snap/Документы/Изображения/Общедоступные/Шаблоны")
+
+'''
+
+'''
+a = input( 'введите логин: ')
+b = input( 'введите пароль: ')
+with open('users.txt', 'w') as f:
+    f.write(f' a:{a} b{b}')
+'''
+
+'''
+with open('w.txt', 'w') as f:
+    f.write(''''''Python is a widely used high-level programming language for general-purpose
+             programming, created by Guido van Rossum and first released in 1991. An interpreted
+             language, Python has a design philosophy that emphasizes code readability (notably
+             using whitespace indentation to delimit code blocks rather than curly brackets or
+             keywords), and a syntax that allows programmers to express concepts in fewer lines of
+             code than might be used in languages such as C++ or Java.'''''')
+
+    if 'w' in f.write():
+        print('da')
+    else:
+        print('net')
+'''
+
+
