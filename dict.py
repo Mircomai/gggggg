d3bcacb4048a81f3054e4b7c2f5b505ec5840d22
AibekYrysbekov <yrysbekovayba@gmail.com>
2022-11-17 19:32:15 +0600

    Helo

--- dict.py
+++ dict.py
@@ -1,7 +1,1 @@
-south_american_countries = ['Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador', 'Guyana', 'Paraguay', 'Peru', 'Suriname', 'Suriname', 'Uruguay', 'Venezuela']
-dic = {}
-un = set(south_american_countries)
-for num in range(len(un)):
-    dic[num+1] = south_american_countries
 
-    print(dic)
